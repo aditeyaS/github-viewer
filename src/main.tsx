50d72f110a5c5aa96c4f1ca5c164a2a5a2b52246
Aditeya Srivastava <this.is.aditeya@gmail.com>
2024-05-28 17:10:28 -0400

    fixing routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { RouterProvider, createHashRouter } from "react-router-dom";
 import Home from "./pages/home";
 import GithubViewer from "./pages/github-viewer";
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Home />,
